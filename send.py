8c629fd5967a81306fdc91dc2a9c52ac02fc775b
athenova <aleksey.sergeyev@yandex.com>
2025-03-02 21:22:39 +0300

    Рефакторинг

--- send.py
+++ send.py
@@ -1,12 +1,5 @@
-import schedule
-import time
 from project import Project
 
 if __name__ == '__main__':
     project = Project()
-    #schedule.every().day.at("12:00",'Europe/Moscow').do(project.send, type='problem')
-    schedule.every().day.at("17:00",'Europe/Moscow').do(project.send, type='solution')
-    two_minutes = 2 * 60
-    for i in range(two_minutes):
-        schedule.run_pending()
-        time.sleep(1)
+    project.send(type='solution')
